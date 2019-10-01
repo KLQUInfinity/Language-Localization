96f8bc9c4e5674bb77adb3a8e43f1231fd020233
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2019-10-01 07:42:58 +0200

    Bendary
    init

---
+++ Assets/ArabicSupport/Scripts/FixText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using ArabicSupport;
+
+[ExecuteInEditMode]
+public class FixText : MonoBehaviour
+{
+    [Multiline]
+    [SerializeField] public string text;
+    [SerializeField] public bool tashkeel = true;
+    [SerializeField] public bool hinduNumbers = true;
+
+    private void Update()
+    {
+        gameObject.GetComponent<Text>().text = ArabicFixer.Fix(text, tashkeel, hinduNumbers);
+    }
+}
+
---
+++ Assets/ArabicSupport/Scripts/FixTextMeshPro.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+using ArabicSupport;
+
+[ExecuteInEditMode]
+public class FixTextMeshPro : MonoBehaviour
+{
+    [Multiline]
+    [SerializeField] public string text;
+    [SerializeField] public bool tashkeel = true;
+    [SerializeField] public bool hinduNumbers = true;
+
+    void Update()
+    {
+        gameObject.GetComponent<TextMeshProUGUI>().text = ArabicFixer.Fix(text, tashkeel, hinduNumbers);
+    }
+
+}
